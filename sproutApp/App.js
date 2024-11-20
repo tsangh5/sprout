3a3b4d274129a68181099a30367e828ff0325683
tsangbe3 <benjamin.tsang@mail.utoronto.ca>
2024-11-19 21:56:26 -0500

    added splash image

--- sproutApp/App.js
+++ sproutApp/App.js
@@ -1,7 +1,7 @@
 import React from 'react';
 import { NavigationContainer } from '@react-navigation/native';
 import { createNativeStackNavigator } from '@react-navigation/native-stack';
-
+import Home from './Home';       // Import your pages
 
 const Stack = createNativeStackNavigator();
 
