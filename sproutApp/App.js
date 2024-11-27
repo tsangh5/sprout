15a09c7ec8814c10ec33f25f12dec239960a5687
Adrian Luedi <luediadrian5@gmail.com>
2024-11-26 22:32:18 -0500

    landingpage

--- sproutApp/App.js
+++ sproutApp/App.js
@@ -1,18 +1,16 @@
 import React from 'react';
 import { NavigationContainer } from '@react-navigation/native';
 import { createNativeStackNavigator } from '@react-navigation/native-stack';
-import ProfileSetup from './pages/ProfileSetup';       // Import your pages
+import { GestureHandlerRootView } from 'react-native-gesture-handler'; // Import GestureHandlerRootView
+import ProfileSetup from './pages/ProfileSetup'; // Import your pages
+import LandingScreen from './components/LandingScreen';
 
 const Stack = createNativeStackNavigator();
 
 export default function App() {
   return (
-    <NavigationContainer>
-      <Stack.Navigator initialRouteName="ProfileSetup">
-        <Stack.Screen name="ProfileSetup" component={ProfileSetup} />
-  
-      </Stack.Navigator>
-    </NavigationContainer>
+    <GestureHandlerRootView style={{ flex: 1 }}>
+      <LandingScreen />
+    </GestureHandlerRootView>
   );
 }
-
